6b28b3db255f005133b9fccef90a16f1c623cec8
Abraham Moreno <adiaz2821@gmail.com>
2020-05-30 00:43:07 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -49,7 +49,7 @@ def over?(board)
 end 
 
 def winner(board)
-  win_combination = win?(board)
+  win_combination = won?(board)
   if win_combination != nil 
     win_location = win_combination[0]
     board[win_location]
